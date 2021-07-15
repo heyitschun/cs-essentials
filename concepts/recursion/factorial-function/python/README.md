d90681c3846a2beca610a2fa0fc3d26e77ea9de1
heyitschun <heyitschun@protonmail.com>
2021-07-15 07:43:57 +0200

    clean up

--- concepts/recursion/factorial-function/python/README.md
+++
@@ -1,6 +0,0 @@
-The *factorial* of positive integer `n` is defined as:
-
-```
-if n = 0 -> 1
-if n > 0 -> n * (n - 1)!
-```
--- concepts/recursion/shortest_paths/go/shortest.go
+++ concepts/recursion/shortest_paths/go/shortest.go
@@ -1,24 +1,9 @@
 package main
 
-import "fmt"
-
-func UniquePaths(m int, n int) int {
-	if m <= 0 || n <= 0 {
-		return 0
-	}
-	return UP(m-1, n-1)
-}
-
-func UP(m int, n int) int {
-	if m < 0 || n < 0 {
-		return 0
-	} else if m == 0 || n == 0 {
+func ShortestPath(m int, n int) int {
+	if m == 1 || n == 1 {
 		return 1
 	}
-	return UP(m-1, n) + UP(m, n-1)
-}
 
-func main() {
-	p := UniquePaths(3, 7)
-	fmt.Println(p)
+	return ShortestPath(m-1, n) + ShortestPath(m, n-1)
 }
